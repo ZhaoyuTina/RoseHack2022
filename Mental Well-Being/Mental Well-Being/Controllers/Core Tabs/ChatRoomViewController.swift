0bb9fdebd0e278f1fd1cedc68147dc323d335d2b
ZhaoyuTina <tinazzy2018@outlook.com>
2022-01-16 12:15:22 -0800

    Third commit

--- Mental Well-Being/Mental Well-Being/Controllers/Core Tabs/ChatRoomViewController.swift
+++ Mental Well-Being/Mental Well-Being/Controllers/Core Tabs/ChatRoomViewController.swift
@@ -6,9 +6,54 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ChatRoomViewController: UIViewController {
 
+
+    @IBOutlet var selectMoodButton: UIButton!
+    @IBOutlet var moodsButtons: [UIButton]!
+    
+    func showButton() {
+        moodsButtons.forEach {button in
+            button.isHidden = !button.isHidden
+            self.view.layoutIfNeeded()
+            
+             
+            
+        }
+    }
+    
+    
+    @IBAction func selectMoodAction(_ sender: Any) {
+        showButton()
+    }
+    
+    @IBAction func moodButtonAction(_ sender: UIButton){
+        showButton()
+        switch sender.currentTitle{
+        case "happy":
+            selectMoodButton.setTitle("happy", for: .normal)
+        
+        case "angry":
+            selectMoodButton.setTitle("angry", for: .normal)
+        
+        case "sad":
+            selectMoodButton.setTitle("sad", for: .normal)
+        
+        case "fear":
+            selectMoodButton.setTitle("fear", for: .normal)
+        
+        case "surprised":
+            selectMoodButton.setTitle("surprised", for: .normal)
+        
+        case "disgust":
+            selectMoodButton.setTitle("disgust", for: .normal)
+        default:
+            selectMoodButton.setTitle("-Select Your Mood-", for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- Mental Well-Being/Mental Well-Being/Controllers/Core Tabs/SearchViewController.swift
+++ Mental Well-Being/Mental Well-Being/Controllers/Core Tabs/SearchViewController.swift
@@ -9,13 +9,60 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    
+    @IBAction func PsychologicalExp(_ sender: Any) {
+        showAlertPsycho()
+    }
+    
+    @IBAction func HealthComm(_ sender: Any) {
+        showAlertHealthComm()
+    }
+    
+    @IBAction func Papers(_ sender: Any) {
+        showAlertPapers()
+    }
+    
+    @IBOutlet weak var UserInputText: UITextField!
+    
+    func showAlertPsycho() {
+        let alert = UIAlertController(title:"Database Chosen",message:"You will hear from Psychological Experts!",preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"Dismiss", style: .cancel, handler: {action in
+            print("tapped dismiss")}))
+        present(alert,animated: true )
+    }
+    
+    func showAlertHealthComm() {
+        let alert = UIAlertController(title:"Database Chosen",message:"You will hear from peers in our Mental Health Community!",preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"Dismiss", style: .cancel, handler: {action in
+            print("tapped dismiss")}))
+        present(alert,animated: true )
+    }
+    
+    func showAlertPapers() {
+        let alert = UIAlertController(title:"Database Chosen",message:"You will read scientific research papers!",preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"Dismiss", style: .cancel, handler: {action in
+            print("tapped dismiss")}))
+        present(alert,animated: true )
+    }
+    
+    func showSearch() {
+        let searchContent = String(UserInputText.text!) ?? "Please fill in search content."
+        let alert = UIAlertController(title:"You are searching for",message:searchContent,preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"Dismiss", style: .cancel, handler: {action in
+            print("tapped dismiss")}))
+        present(alert,animated: true )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func Search(_ sender: Any) {
+        showSearch()
+    }
+    
     /*
     // MARK: - Navigation
 
