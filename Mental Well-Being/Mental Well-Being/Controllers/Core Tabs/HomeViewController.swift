d7c811e697477e5001fa63bdf7771f53267711c6
ZhaoyuTina <tinazzy2018@outlook.com>
2022-01-15 16:25:47 -0800

    First commit

---
+++ Mental Well-Being/Mental Well-Being/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mental Well-Being
+//
+//  Created by Zhaoyu Zhang on 1/15/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Mental Well-Being/Mental Well-BeingUITests/Mental_Well_BeingUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Mental_Well_BeingUITestsLaunchTests.swift
+//  Mental Well-BeingUITests
+//
+//  Created by Zhaoyu Zhang on 1/15/22.
+//
+
+import XCTest
+
+class Mental_Well_BeingUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
